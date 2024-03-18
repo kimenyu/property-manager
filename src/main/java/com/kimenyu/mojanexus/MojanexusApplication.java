a47132ac6f5249697cfc69c2805af07fbd80c358
kimenyu <njorogekimenyu@gmail.com>
2024-03-18 09:39:03 +0300

    first commit

---
+++ src/main/java/com/kimenyu/mojanexus/MojanexusApplication.java
@@ -0,0 +1,13 @@
+package com.kimenyu.mojanexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojanexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojanexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimenyu/mojanexus/MojanexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimenyu.mojanexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojanexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
